a7b6338403190145657468034a1bff428c0aa4ba
mwest71 <p.charatonik@gmail.com>
2013-06-07 21:09:00 +0200

    hehe zmiana banana

--- Flatmates/main.m
+++ Flatmates/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  Flatmates
 //
-//  Created by Michał on 04.06.2013.
+//  Created by Michał Burczymucha on 04.06.2013.
 //  Copyright (c) 2013 Michał Ciesielski. All rights reserved.
 //
 
